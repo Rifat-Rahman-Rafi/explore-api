1b31643d238aabdc0b2df572523642b9d2fa93e0
Rifat-Rahman-Rafi <rifatrahmanrafi47@gmail.com>
2023-03-01 01:19:25 +0600

    api

--- js/fetch.js
+++ js/fetch.js
@@ -8,3 +8,17 @@ function loadData()
       .then(response => response.json())
       .then(json => console.log(json))
 }
+function loadUser(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+      .then(data => displayData(data))
+}
+
+function displayData(data){
+    const ul= document.getElementById('users-list')
+    for(const user of data){
+        const li = document.createElement('li');
+        li.innerText=user.name;
+        ul.appendChild(li);
+    }
+}
